c27c4603156214203addf392aab9d6cca5ae5112
Mohammed-Almuziny <mohammed.abdullah.js@tuwaiq.edu.sa>
2021-11-25 16:48:42 +0300

    update create page

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
         <Route exact path="/quizzes" element={<Quizzes />} />
         <Route exact path="/quiz/:id" element={<Quiz />} />
         <Route exact path="/history" element={<History />} />
-        <Route exact path="/create" element={<Create />} />
+        <Route exact path="/create" element={<Create user={user} />} />
         <Route exact path="/create/quiz" element={<CreateQuiz />} />
         <Route exact path="/SignUp" element={<SignUp />} />
         <Route
@@ -31,7 +31,7 @@ function App() {
           path="/LogIn"
           element={<LogIn user={user} setUser={setUser} />}
         />
-        <Route exact path="/Setting" element={<Setting user={user}/>} />
+        <Route exact path="/Setting" element={<Setting user={user} />} />
       </Routes>
     </div>
   );
--- src/components/Create/index.jsx
+++ src/components/Create/index.jsx
@@ -1,13 +1,76 @@
-import React from 'react';
-import {Link ,Typography, Container} from "@mui/material"
+import { React, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import {
+  Container,
+  Typography,
+  Grid,
+  Card,
+  CardContent,
+  CardActions,
+  Button,
+  Link,
+} from "@mui/material";
+import CircularProgress from "@mui/material/CircularProgress";
 
-export const Create = () => {
-  return (
-    <div>
-      <Container>
-        <Link  color="inherit" underline="none" href="/create/quiz">create new quize</Link>
-        <Typography align="center" variant="h2">your Quiz</Typography>
-      </Container>
-    </div>
-  )
-}
+export const Create = ({ user }) => {
+  const [quizzes, setQuizzes] = useState([]);
+
+  const navigate = useNavigate();
+
+  const getData = async () => {
+    const result = await axios.get(
+      `${process.env.REACT_APP_BASE_URL}/users/Creations/${user}`
+    );
+    setQuizzes(result.data);
+  };
+
+  const handleClick = (id) => {
+    navigate(`/quiz/${id}`);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+  return quizzes[0] ? (
+    <Container>
+      <Link color="inherit" underline="none" href="/create/quiz">
+        create new quize
+      </Link>{" "}
+      <Typography align="center" variant="h2">
+        your Quiz
+      </Typography>
+      <Grid container spacing={2}>
+        {quizzes.map((quiz, i) => (
+          <Grid item lg={3} md={4} sm={6} xs={12} key={i}>
+            <Card>
+              <CardContent>{quiz.title}</CardContent>
+              <CardActions>
+                {" "}
+                <Button
+                  onClick={() => {
+                    handleClick(quiz._id);
+                  }}
+                >
+                  {" "}
+                  start{" "}
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  ) : (
+    <Container sx={{ mx: "auto", width: 200 }}>
+      <CircularProgress />
+    </Container>
+  );
+
+  // <div>
+  //   <Container>
+  //     <Link  color="inherit" underline="none" href="/create/quiz">create new quize</Link>
+  //     <Typography align="center" variant="h2">your Quiz</Typography>
+  //   </Container>
+  // </div>
+};
